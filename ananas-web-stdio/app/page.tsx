a32cc23d712476e3dd62d29917e5e75f0c2dd7dc
Your Name <you@example.com>
2024-01-24 23:59:25 +0700

    fix bug đeloy vercel

--- ananas-web-stdio/app/page.tsx
+++ ananas-web-stdio/app/page.tsx
@@ -2,18 +2,14 @@
 import React from 'react';
 import { Layout } from "antd";
 import MainContent from '@/components/mainContent/MainContent';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function Home() {
 
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<MainContent  />} />
-        </Routes>
-      </Layout>
-    </Router>
+ 
+    <Layout>
+    <MainContent  />
+    </Layout>
   );
 }
 
